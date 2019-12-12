86dfafafa5eb5212b9206dc8f22389696b391bdf
Komei Nomura <komei.nomura@pepabo.com>
2019-12-12 18:47:21 +0900

    change stack.shift
    stack.pop is deep_first_serch method

--- other/breadth_first_search.rb
+++ other/breadth_first_search.rb
@@ -1,9 +1,11 @@
+# アルファベット A, B, C からなる、3文字以下の文字列の組み合わせをすべて取得する。
+
 def bfs(patterns)
   # スタックとなる配列を用意し、初期状態である空文字列を格納する
   stack = [""]
   until stack.empty?
     # スタックから1つ取り出す（末尾から取り出すのでpopを使う）
-    str = stack.pop
+    str = stack.shift
 
     # 停止条件: 文字列調査3文字より大きい
     next if str.length > 3
